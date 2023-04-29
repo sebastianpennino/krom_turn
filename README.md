c8a2ed6d61ffc9f4d14332502f6ae6650b7a054b
sebastianpennino <sebastian.pennino@gmail.com>
2023-04-29 14:39:46 -0300

    Initial commit. Basic scaffolding

---
+++ README.md
@@ -0,0 +1,10 @@
+# Kromsys Turn Tracker
+PWA Kromsys turn tracker
+
+# Running the project locally
+```
+  yarn dev
+```
+
+# Tailwind cheat sheet
+`https://nerdcave.com/tailwind-cheat-sheet`
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { ReactComponent as KromsysLogo } from "./assets/k-logo.svg";
+
+const App = () => {
+  return (
+    <div>
+      Hello <KromsysLogo />
+    </div>
+  );
+};
+export default App;
