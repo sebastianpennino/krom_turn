3bd87ca34e4c53211fb3fb4a35931dba8bc33635
sebastianpennino <sebastian.pennino@gmail.com>
2023-05-05 20:18:29 -0300

    Second iteration

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -35,7 +35,32 @@ const CharacterCard: React.FC<CharacterCardProps> = ({
     <div className="bg-neutral-950 rounded-lg shadow-lg flex">
       <div className="w-9/12 p-2">
         <h3 className="text-sm">{character.name}</h3>
-        <div className="mt-2 flex space-x-2">
+        <div className="mt-2 space-x-2 text-sm">
+          <button
+            className="bg-red-800 px-2 py-0 focus:outline-none"
+            onClick={handleRedClick}
+            disabled={disabled}
+          >
+            255
+          </button>
+          <button
+            className="bg-blue-800 px-2 py-0 focus:outline-none"
+            onClick={handleBlueClick}
+            disabled={disabled}
+          >
+            255
+          </button>
+          <button
+            className="bg-lime-800 px-2 py-0 focus:outline-none"
+            onClick={handleBlueClick}
+            disabled={disabled}
+          >
+            255
+          </button>
+        </div>
+      </div>
+      <div className="w-3/12">
+        <div className="flex flex-col">
           <button
             className="bg-red-800 flex space-x-2 p-2 focus:outline-none"
             onClick={handleRedClick}
@@ -71,46 +96,6 @@ const CharacterCard: React.FC<CharacterCardProps> = ({
                 className="h-4 w-4 bg-blue-950 rounded-full"
               ></div>
             ))}
-          </button>
-          <button
-            className="bg-lime-800 flex space-x-2 p-2 focus:outline-none"
-            onClick={handleBlueClick}
-            disabled={disabled}
-          >
-            {Array.from({ length: character.prPoints }, (_, index) => (
-              <div
-                key={index}
-                className="h-4 w-4 bg-lime-600 rounded-full"
-              ></div>
-            ))}
-            {Array.from({ length: max - character.prPoints }, (_, index) => (
-              <div
-                key={index}
-                className="h-4 w-4 bg-lime-950 rounded-full"
-              ></div>
-            ))}
-          </button>
-        </div>
-      </div>
-      <div className="w-3/12">
-        <div className="text-sm h-full">
-          <button
-            className="w-1/2 bg-red-700 h-1/2 p-0 rounded-r-none rounded-b-none"
-            disabled={disabled}
-          >
-            255
-          </button>
-          <button
-            className="w-1/2 bg-blue-700 h-1/2 p-0 rounded-l-none rounded-b-none"
-            disabled={disabled}
-          >
-            255
-          </button>
-          <button
-            className="w-full bg-lime-700 h-1/2 p-0 rounded-t-none"
-            disabled={disabled}
-          >
-            255
           </button>
         </div>
       </div>
